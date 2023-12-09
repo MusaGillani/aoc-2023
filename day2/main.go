42129aaefcc65fa74114377f9f98c29e3f0f963c
Musa Gillani <musagillaniwork@gmail.com>
2023-12-10 03:25:09 +0500

    completed part 2 with sum of power of max rgbs in all games

--- day2/main.go
+++ day2/main.go
@@ -24,6 +24,38 @@ func sum(arr []int) int {
 	return total
 }
 
+func maxValueInSubsets(subsets string) map[string]int {
+	maxValues := map[string]int{
+		"r": 1,
+		"g": 1,
+		"b": 1,
+	}
+	for _, subset := range strings.Split(subsets, ";") {
+		colors := strings.Split(subset, ",")
+		for _, color := range colors {
+			countColor := strings.Split(color, " ")
+			count, err := strconv.Atoi(countColor[1])
+			check(err)
+			color := countColor[2]
+			switch color {
+			case "red":
+				if count > maxValues["r"] {
+					maxValues["r"] = count
+				}
+			case "green":
+				if count > maxValues["g"] {
+					maxValues["g"] = count
+				}
+			case "blue":
+				if count > maxValues["b"] {
+					maxValues["b"] = count
+				}
+			}
+		}
+	}
+	return maxValues
+}
+
 func subsetsPassed(subsets string) bool {
 	passed := true
 SubsetsLoop:
@@ -61,15 +93,20 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 
 	var passedGameIds []int
+	var powers []int
 	for scanner.Scan() {
 		input := strings.Split(scanner.Text(), ":")
 		gameId, subsets := input[0], input[1]
 		id, err := strconv.Atoi(strings.Split(gameId, " ")[1])
 		check(err)
-		// push all gameIds and skip the ones that fail
-		if subsetsPassed(subsets) {
+		// push all gameIds and "with" the ones that fail
+		gamePassed, maxValues := subsetsPassed(subsets), maxValueInSubsets(subsets)
+		powers = append(powers, maxValues["r"]*maxValues["g"]*maxValues["b"])
+		if gamePassed {
 			passedGameIds = append(passedGameIds, id)
 		}
 	}
-	fmt.Printf("total: %d\n", sum(passedGameIds))
+	part1, part2 := sum(passedGameIds), sum(powers)
+	fmt.Printf("total part1: %d\n", part1)
+	fmt.Printf("total part2: %d\n", part2)
 }
